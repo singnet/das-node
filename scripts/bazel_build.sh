bec1152f662488c8264f9fe5c60d9a8403bbfc0e
Pedro Costa <pedro@rubberduck.local>
2024-09-19 15:05:36 -0300

    updating CMakeLists.txt and scripts/bazel_build.sh

--- scripts/bazel_build.sh
+++ scripts/bazel_build.sh
@@ -3,14 +3,27 @@
 /opt/bazel/bazelisk build --jobs 6 --noenable_bzlmod //cache_node:cache_node_lib
 
 ##### Current workaround to build nanobind with CMake ######
+# removes folder ../bin if exists
+if [ -d "../bin" ]; then 
+  rm -rf ../bin 
+fi
+if [ -d "../bazel_assets" ]; then 
+  rm -rf ../bazel_assets 
+fi
+
+# Recreates folders
+mkdir -p ../bin/
+mkdir -p ../bazel_assets/
+
+# Copy files
 cp -r bazel-bin/* ../bin/
-cp -r /root/.cache/bazel/_bazel_root/ee73ef8bd6049ae309312492ee2f22da/external/com_google_absl/absl/ ../bazel_assets/
-cp -r /root/.cache/bazel/_bazel_root/f400afec2d24eb3a6eb5cb42df4b7b24/external/com_github_grpc_grpc/include/grpcpp/ ../bazel_assets
-cp -r /root/.cache/bazel/_bazel_root/f400afec2d24eb3a6eb5cb42df4b7b24/external/com_github_grpc_grpc/include/grpc ../bazel_assets/
-cp /root/.cache/bazel/_bazel_root/f400afec2d24eb3a6eb5cb42df4b7b24/execroot/__main__/bazel-out/k8-fastbuild/bin/external/com_github_singnet_das_proto/atom_space_node.pb.h ../bazel_assets/
-cp /root/.cache/bazel/_bazel_root/f400afec2d24eb3a6eb5cb42df4b7b24/execroot/__main__/bazel-out/k8-fastbuild/bin/external/com_github_singnet_das_proto/atom_space_node.grpc.pb.h ../bazel_assets/
-cp -r /root/.cache/bazel/_bazel_root/f400afec2d24eb3a6eb5cb42df4b7b24/external/com_google_protobuf/src/google/ ../bazel_assets/
-cp /root/.cache/bazel/_bazel_root/f400afec2d24eb3a6eb5cb42df4b7b24/execroot/__main__/bazel-out/k8-fastbuild/bin/external/com_github_singnet_das_proto/common.pb.h ../bazel_assets/
+cp -r bazel-src/external/com_google_absl/absl/ ../bazel_assets/
+cp -r bazel-src/external/com_github_grpc_grpc/include/grpcpp/ ../bazel_assets
+cp -r bazel-src/external/com_github_grpc_grpc/include/grpc ../bazel_assets/
+cp -r bazel-src/external/com_google_protobuf/src/google/ ../bazel_assets/
+cp bazel-bin/external/com_github_singnet_das_proto/atom_space_node.pb.h ../bazel_assets/
+cp bazel-bin/external/com_github_singnet_das0_proto/atom_space_node.grpc.pb.h ../bazel_assets/
+cp bazel-bin/external/com_github_singnet_das_proto/common.pb.h ../bazel_assets/
 ##### ---- ######
 
 # /opt/bazel/bazelisk clean
