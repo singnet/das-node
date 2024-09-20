d5c78f9d70248b0df4e0ebe352b146c7ecfc46a0
Pedro Costa <pedro@rubberduck.local>
2024-09-20 13:06:18 -0300

    scripts/bazel_build.sh incorporate ar rcs commands

--- scripts/bazel_build.sh
+++ scripts/bazel_build.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 
-/opt/bazel/bazelisk build --jobs 6 --noenable_bzlmod :hyperon_das_node #//cache_node:cache_node_lib
+/opt/bazel/bazelisk build --jobs 16 --noenable_bzlmod ...
 
-##### Current workaround to build nanobind with CMake ######
-cp -R bazel-bin/* ../bin/
-cp -R /root/.cache/bazel/_bazel_root/ee73ef8bd6049ae309312492ee2f22da/external/com_google_absl/absl ../bazel_assets/
-cp -r /root/.cache/bazel/_bazel_root/f400afec2d24eb3a6eb5cb42df4b7b24/external/com_github_grpc_grpc/include/grpcpp/ ../bazel_assets
-cp -r /root/.cache/bazel/_bazel_root/f400afec2d24eb3a6eb5cb42df4b7b24/external/com_github_grpc_grpc/include/grpc ../bazel_assets/
-cp /root/.cache/bazel/_bazel_root/f400afec2d24eb3a6eb5cb42df4b7b24/execroot/__main__/bazel-out/k8-fastbuild/bin/external/com_github_singnet_das_proto/atom_space_node.pb.h ../bazel_assets/
-cp /root/.cache/bazel/_bazel_root/f400afec2d24eb3a6eb5cb42df4b7b24/execroot/__main__/bazel-out/k8-fastbuild/bin/external/com_github_singnet_das_proto/atom_space_node.grpc.pb.h ../bazel_assets/
-cp -r /root/.cache/bazel/_bazel_root/f400afec2d24eb3a6eb5cb42df4b7b24/external/com_google_protobuf/src/google/ ../bazel_assets/
-cp /root/.cache/bazel/_bazel_root/f400afec2d24eb3a6eb5cb42df4b7b24/execroot/__main__/bazel-out/k8-fastbuild/bin/external/com_github_singnet_das_proto/common.pb.h ../bazel_assets/
+##### Workaround to build nanobind with CMake ######
+# removes folder if exists
+if [ -d "../bazel_assets" ]; then 
+  rm -rf ../bazel_assets 
+fi
+
+# Recreates folders
+mkdir -p ../bazel_assets/
+
+#Build external libs to bazel assets
+ar rcs ../bazel_assets/libexternal.a \
+  $(find bazel-bin/external/ -iname "*.o" -o -iname "*.lo" -o -iname "*.a")
+
+#Build internal libs to bazel assets
+# internal dirs of bazel-bin excliding external folder
+ar rcs ../bazel_assets/libinternal.a \
+  $(find bazel-bin/ -not -path 'bazel-bin/external/*' -iname "*o" -o -iname "*.lo" -o -iname "*.a")
+
+
+# Copy files
+cp -r bazel-src/external/com_github_grpc_grpc/include/grpc ../bazel_assets/
+cp -r bazel-src/external/com_github_grpc_grpc/include/grpcpp/ ../bazel_assets
+cp -r bazel-src/external/com_google_absl/absl/ ../bazel_assets/
+cp -r bazel-src/external/com_google_protobuf/src/google/ ../bazel_assets/
+cp bazel-bin/external/com_github_singnet_das_proto/atom_space_node.grpc.pb.h ../bazel_assets/
+cp bazel-bin/external/com_github_singnet_das_proto/atom_space_node.pb.h ../bazel_assets/
+cp bazel-bin/external/com_github_singnet_das_proto/common.pb.h ../bazel_assets/
+
 ##### ---- ######
 
 # /opt/bazel/bazelisk clean
