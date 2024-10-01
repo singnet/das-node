3bc9413b8421347a11949fa03fb104ed145fa669
Andre Senna <andre.senna@gmail.com>
2024-10-01 18:17:25 -0300

    Added a test case for custom Message exchanges

--- tests/test_atom_space_node.py
+++ tests/test_atom_space_node.py
@@ -10,10 +10,11 @@ class TestMessage(Message):
         command: str,
         args: List[str]):
 
+        super().__init__()
         self.command = command
         self.args = args
 
-    def act(node: AtomSpaceNode):
+    def act(self, node: AtomSpaceNode):
         node.command = self.command
         node.args = self.args
 
@@ -36,6 +37,8 @@ class TestNode(AtomSpaceNode):
         else:
             self.add_peer(server_id)
             self.server_id = server_id
+        self.command = ""
+        self.args = []
 
     def cast_leadership_vote(self) -> str:
         if self.is_server:
@@ -106,3 +109,54 @@ class TestAtomSpaceNode(TestCase):
             assert client1.node_id() == client1_id
             assert client2.node_id() == client2_id
 
+class TestAtomSpaceNode(TestCase):
+
+    def test_communication(self):
+
+        server_id = "localhost:30700"
+        client1_id = "localhost:30701"
+        client2_id = "localhost:30702"
+
+        for messaging_type in [MessageBrokerType.RAM, MessageBrokerType.GRPC]:
+
+            server = TestNode(server_id, server_id, LeadershipBrokerType.SINGLE_MASTER_SERVER, messaging_type, True)
+            client1 = TestNode(client1_id, server_id, LeadershipBrokerType.SINGLE_MASTER_SERVER, messaging_type, False)
+            client2 = TestNode(client2_id, server_id, LeadershipBrokerType.SINGLE_MASTER_SERVER, messaging_type, False)
+
+            server.join_network()
+            client1.join_network()
+            client2.join_network()
+            time.sleep(1)
+
+            assert server.command == ""
+            assert server.args == []
+            assert client1.command == ""
+            assert client1.args == []
+            assert client2.command == ""
+            assert client2.args == []
+
+            assert server.node_joined_network_count == 2
+            assert client1.node_joined_network_count == 1
+            assert client2.node_joined_network_count == 0
+
+            args1 = ["a", "b"]
+            server.broadcast("c1", args1)
+            time.sleep(1)
+
+            assert server.command == ""
+            assert server.args == []
+            assert client1.command == "c1"
+            assert client1.args == args1
+            assert client2.command == "c1"
+            assert client2.args == args1
+
+            args2 = ["a2", "b2"]
+            server.send("c2", args2, client1_id);
+            time.sleep(1);
+
+            assert server.command == ""
+            assert server.args == []
+            assert client1.command == "c2"
+            assert client1.args == args2
+            assert client2.command == "c1"
+            assert client2.args == args1
