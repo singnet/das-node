654275640036b01ebe00a11dfca1dd374239d107
Pedro Borges Costa <pedrobc89@gmail.com>
2024-10-03 11:48:01 -0300

    [#14] Repo Cleanup (#27)
    * remove deprecated python code
    
    * remove cache_node
    
    * remove unused scripts
    
    * test cache node example
    
    * cleanup scripts
    
    ---------

--- scripts/compile_protos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PROTOS=("echo" "common" "node")
-
-for proto in ${PROTOS[@]}; do
-    protoc -I/opt/proto --cpp_out=/opt/grpc /opt/proto/${proto}.proto
-    cd /opt/grpc
-    gcc -c ${proto}.pb.cc
-done
