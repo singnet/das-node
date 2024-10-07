1b130111da0f5c8ae2485150b8d523cb11073c67
levisingularity <rafael.levi@singularitynet.io>
2024-10-07 17:26:06 -0300

    das-node-29: Update container tty script

--- scripts/container_tty.sh
+++ scripts/container_tty.sh
@@ -2,13 +2,19 @@
 
 CONTAINER_NAME="das-node-bash"
 
+if [ -n "$1" ]; then
+    CMD="$1"
+else
+    CMD="bash"
+fi
+
 docker run \
     --net="host" \
     --name=$CONTAINER_NAME \
     --volume .:/opt/hyperon_das_node \
     --volume /tmp:/tmp \
     -it das-node-builder \
-    bash
+   bash -c "$CMD"
 
 sleep 1
 docker rm $CONTAINER_NAME >& /dev/null
