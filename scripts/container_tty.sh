8f356136908db11a5bfac83abfc15f108ff5c008
Levi <141682181+levisingularity@users.noreply.github.com>
2024-10-15 15:47:12 -0300

    [#29] Implement pipeline for building and publishing the Hyperon DAS Node package (#36)
    * das-node-29: Add pipeline
    
    * das-node-29: Build das node image
    
    * das-node-29: Update container tty script
    
    * das-node-29: update pipeline
    
    * das-node-29: comment tag
    
    * das-node-29: scritps typo
    
    * das-node-29: publish pypi
    
    * das-node-29: update container_tty
    
    * das-node-29: publish package
    
    * das-node-29: update twine path
    
    * das-node-29: fix twine upload
    
    * das-node-29: Update working directory
    
    * das-node-29: Update entrypoint
    
    * das-node-29: Update version

--- scripts/container_tty.sh
+++ scripts/container_tty.sh
@@ -2,13 +2,21 @@
 
 CONTAINER_NAME="das-node-bash"
 
+if [ -n "$1" ]; then
+    CMD="$1"
+    INTERACTIVE="-i" 
+else
+    CMD="bash"
+    INTERACTIVE="-it" 
+fi
+
 docker run \
     --net="host" \
     --name=$CONTAINER_NAME \
     --volume .:/opt/hyperon_das_node \
     --volume /tmp:/tmp \
-    -it das-node-builder \
-    bash
+    $INTERACTIVE das-node-builder \
+    bash -c "$CMD"
 
 sleep 1
 docker rm $CONTAINER_NAME >& /dev/null
