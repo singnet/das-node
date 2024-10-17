21917197b6659e205aaeecf9fc828de3ecbfc53f
Pedro Costa <pedrobc89@gmail.com>
2024-10-17 12:02:49 -0300

    reviewer's comments

--- scripts/run_wheeler.sh
+++ scripts/run_wheeler.sh
@@ -3,10 +3,10 @@
 CONTAINER_NAME="das-node-wheeler"
 
 docker run \
+    --rm \
     --name=$CONTAINER_NAME \
+    -e _USER=$(id -u) \
+    -e GROUP=$(id -g) \
     --volume .:/opt/hyperon_das_node \
     --workdir /opt/hyperon_das_node \
     das-node-wheeler
-
-sleep 1
-docker rm $CONTAINER_NAME
